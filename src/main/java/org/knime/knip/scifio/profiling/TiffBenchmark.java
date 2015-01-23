428fc04c3a12452703de773a05296a3eee492244
Gabriel Einsdorf <gabriel.einsdorf@uni-konstanz.de>
2015-01-23 19:24:51 +0100

    WIP

--- src/main/java/org/knime/knip/scifio/profiling/TiffBenchmark.java
+++ src/main/java/org/knime/knip/scifio/profiling/TiffBenchmark.java
@@ -1,18 +1,25 @@
 package org.knime.knip.scifio.profiling;
 
+import io.scif.Format;
+import io.scif.FormatException;
+import io.scif.SCIFIO;
 import io.scif.img.ImgIOException;
 import io.scif.img.ImgOpener;
 import io.scif.img.ImgSaver;
 import io.scif.img.SCIFIOImgPlus;
-import net.imglib2.exception.IncompatibleTypeException;
-import net.imglib2.img.Img;
-import org.junit.Before;
-import org.junit.Test;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
+import net.imglib2.exception.IncompatibleTypeException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.scijava.Context;
 
 /**
  * Created by Gabriel on 06/12/2014.
@@ -24,28 +31,34 @@ public class TiffBenchmark {
     private static String BASENAME = "write_";
     private static String FORMAT = ".tif";
 
+    /* map from the writer names to the actual writers */
+    private HashMap<String, Format> m_mapFormats = null;
+    private final io.scif.SCIFIO SCIFIO = new SCIFIO();
+    private ImgSaver m_saver;
+    private String[] m_writers;
+
     @Before
     public void setup() throws IncompatibleTypeException, ImgIOException {
 
-        //cleanup
-        Path folder = Paths.get(BASEFOLDER);
+        // cleanup
+        final Path folder = Paths.get(BASEFOLDER);
         folder.toFile().delete();
         folder.toFile().mkdirs();
     }
 
+
     @Test
-    public void tiffTest() throws ImgIOException, IncompatibleTypeException {
+    public void tiffNaiveTest() throws ImgIOException,
+            IncompatibleTypeException, FormatException {
 
-        ImgOpener opener = new ImgOpener();
-        ImgSaver saver = new ImgSaver();
-        List<SCIFIOImgPlus<?>> imgs;
-        imgs = opener.openImgs(IMG1);
-        SCIFIOImgPlus<?> sfimg = imgs.get(0);
-        Img<?> img = sfimg.getImg();
+        final ImgOpener opener = new ImgOpener();
+        List<SCIFIOImgPlus<?>> imgs = opener.openImgs(IMG1);
+        final SCIFIOImgPlus<?> sfimg = imgs.get(0);
+//        final Img<?> img = sfimg.getImg();
 
-        int numWrites = 100;
+        final int numWrites = 100;
         for (int i = 0; i < numWrites; i++) {
-            saver.saveImg(BASEFOLDER + BASENAME + i + FORMAT, img);
+            m_saver.saveImg(BASEFOLDER + BASENAME + i + FORMAT, sfimg);
         }
     }
 
