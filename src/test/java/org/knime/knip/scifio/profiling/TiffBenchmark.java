70bedc379a8f93a47ccc85887036177fca1d81b9
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-01-24 17:21:48 +0100

    Fix test image access
    The test image was moved, but the test code was not adjusted (this
    developer's fault). This change fixes that.
    
    While at it, avoid the completely unnecessary use of Java 1.7 API.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src/test/java/org/knime/knip/scifio/profiling/TiffBenchmark.java
+++ src/test/java/org/knime/knip/scifio/profiling/TiffBenchmark.java
@@ -1,13 +1,13 @@
 package org.knime.knip.scifio.profiling;
 
+import static org.junit.Assert.assertTrue;
 import io.scif.FormatException;
 import io.scif.img.ImgIOException;
 import io.scif.img.ImgOpener;
 import io.scif.img.ImgSaver;
 import io.scif.img.SCIFIOImgPlus;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.File;
 import java.util.List;
 
 import net.imglib2.exception.IncompatibleTypeException;
@@ -15,13 +15,15 @@ import net.imglib2.exception.IncompatibleTypeException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.scijava.util.FileUtils;
 
 public class TiffBenchmark {
 
-	private static final String BASEFOLDER = "writetest/target/";
-	private static final String IMG1 = "writetest/res/FakeTracks.tif";
+	private static final String IMG1 = "/FakeTracks.tif";
 	private static final String BASENAME = "write_";
 	private static final int NUM_WRITES = 300;
+
+	private File baseDirectory;
 
 	private final ImgSaver m_saver = new ImgSaver();
 	private SCIFIOImgPlus<?> m_sfimg;
@@ -30,19 +32,19 @@ public class TiffBenchmark {
 	public void setup() throws IncompatibleTypeException, ImgIOException {
 
 		// cleanup
-		final Path folder = Paths.get(BASEFOLDER);
-		folder.toFile().delete();
-		folder.toFile().mkdirs();
+		baseDirectory = new File(getClass().getResource("/").getPath(), "../output");
+		FileUtils.deleteRecursively(baseDirectory);
+		assertTrue(baseDirectory.mkdirs());
 
+		final File testImage = new File(getClass().getResource(IMG1).getPath());
 		final ImgOpener opener = new ImgOpener();
-		List<SCIFIOImgPlus<?>> imgs = opener.openImgs(IMG1);
+		List<SCIFIOImgPlus<?>> imgs = opener.openImgs(testImage.getPath());
 		m_sfimg = imgs.get(0);
 	}
 	
 	@After
 	public void cleanup() throws Exception {
-		final Path folder = Paths.get(BASEFOLDER);
-		folder.toFile().delete();
+		FileUtils.deleteRecursively(baseDirectory);
 	}
 
 	@Test
@@ -50,7 +52,7 @@ public class TiffBenchmark {
 			IncompatibleTypeException, FormatException {
 
 		for (int i = 0; i < NUM_WRITES; i++) {
-			m_saver.saveImg(BASEFOLDER + BASENAME + i + ".tif", m_sfimg);
+			m_saver.saveImg(new File(baseDirectory, BASENAME + i + ".tif").getPath(), m_sfimg);
 		}
 	}
 
@@ -59,7 +61,7 @@ public class TiffBenchmark {
 			IncompatibleTypeException, FormatException {
 
 		for (int i = 0; i < NUM_WRITES; i++) {
-			m_saver.saveImg(BASEFOLDER + BASENAME + i + ".png", m_sfimg);
+			m_saver.saveImg(new File(baseDirectory, BASENAME + i + ".png").getPath(), m_sfimg);
 		}
 	}
 
@@ -68,7 +70,7 @@ public class TiffBenchmark {
 			IncompatibleTypeException, FormatException {
 
 		for (int i = 0; i < NUM_WRITES; i++) {
-			m_saver.saveImg(BASEFOLDER + BASENAME + i + ".eps", m_sfimg);
+			m_saver.saveImg(new File(baseDirectory, BASENAME + i + ".eps").getPath(), m_sfimg);
 		}
 	}
 
@@ -77,7 +79,7 @@ public class TiffBenchmark {
 			IncompatibleTypeException, FormatException {
 
 		for (int i = 0; i < NUM_WRITES; i++) {
-			m_saver.saveImg(BASEFOLDER + BASENAME + i + ".ics", m_sfimg);
+			m_saver.saveImg(new File(baseDirectory, BASENAME + i + ".ics").getPath(), m_sfimg);
 		}
 	}
 
@@ -86,7 +88,7 @@ public class TiffBenchmark {
 			IncompatibleTypeException, FormatException {
 
 		for (int i = 0; i < NUM_WRITES; i++) {
-			m_saver.saveImg(BASEFOLDER + BASENAME + i + ".jpg", m_sfimg);
+			m_saver.saveImg(new File(baseDirectory, BASENAME + i + ".jpg").getPath(), m_sfimg);
 		}
 	}
 
