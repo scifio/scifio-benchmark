78b25e798ac50ae56e3cea413c9e65244ff75bb4
Gabriel Einsdorf <gabriel.einsdorf@uni-konstanz.de>
2015-04-17 18:17:39 +0200

    WIP

--- src/test/java/org/knime/knip/scifio/profiling/ReadTest.java
+++ src/test/java/org/knime/knip/scifio/profiling/ReadTest.java
@@ -8,13 +8,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -53,7 +53,7 @@ import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
 import com.carrotsearch.junitbenchmarks.BenchmarkRule;
 
 @BenchmarkOptions(benchmarkRounds = 200, warmupRounds = 5)
-public class WriteTest {
+public class ReadTest {
 
 	/** Needed for JUnit-Benchmarks */
 	@Rule
@@ -70,17 +70,12 @@ public class WriteTest {
 	@Before
 	public void setup() throws IncompatibleTypeException, ImgIOException {
 
-		// cleanup
-		baseDirectory = new File(getClass().getResource("/").getPath(), "../output");
-		FileUtils.deleteRecursively(baseDirectory);
-		assertTrue(baseDirectory.mkdirs());
-
 		final File testImage = new File(getClass().getResource(IMG1).getPath());
 		final ImgOpener opener = new ImgOpener();
 		List<SCIFIOImgPlus<?>> imgs = opener.openImgs(testImage.getPath());
 		m_sfimg = imgs.get(0);
 	}
-	
+
 	@After
 	public void cleanup() throws Exception {
 		FileUtils.deleteRecursively(baseDirectory);
