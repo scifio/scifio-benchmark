554fc45b23fc3208cd3b7d57f72640ee0339ac1d
Gabriel Einsdorf <gabriel.einsdorf@uni-konstanz.de>
2015-05-12 15:46:54 +0200

    Add tests for more formats and  sequential writing mode for each format.

--- src/test/java/org/knime/knip/scifio/profiling/WriteTest.java
+++ src/test/java/org/knime/knip/scifio/profiling/WriteTest.java
@@ -32,6 +32,7 @@ package org.knime.knip.scifio.profiling;
 
 import static org.junit.Assert.assertTrue;
 import io.scif.FormatException;
+import io.scif.config.SCIFIOConfig;
 import io.scif.img.ImgIOException;
 import io.scif.img.ImgOpener;
 import io.scif.img.ImgSaver;
@@ -42,10 +43,7 @@ import java.util.List;
 
 import net.imglib2.exception.IncompatibleTypeException;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.rules.TestRule;
 import org.scijava.util.FileUtils;
 
@@ -66,6 +64,8 @@ public class WriteTest {
 
 	private final ImgSaver m_saver = new ImgSaver();
 	private SCIFIOImgPlus<?> m_sfimg;
+	private SCIFIOConfig m_sequentialConfig = new SCIFIOConfig();
+
 
 	@Before
 	public void setup() throws IncompatibleTypeException, ImgIOException {
@@ -75,6 +75,9 @@ public class WriteTest {
 		FileUtils.deleteRecursively(baseDirectory);
 		assertTrue(baseDirectory.mkdirs());
 
+		m_sequentialConfig.writerSetSequential(true);
+
+		// Read test image
 		final File testImage = new File(getClass().getResource(IMG1).getPath());
 		final ImgOpener opener = new ImgOpener();
 		List<SCIFIOImgPlus<?>> imgs = opener.openImgs(testImage.getPath());
@@ -86,11 +89,33 @@ public class WriteTest {
 		FileUtils.deleteRecursively(baseDirectory);
 	}
 
-//	@Test
+	@Test
 	public void tiffNaiveTest() throws ImgIOException,
 			IncompatibleTypeException, FormatException {
 
 		m_saver.saveImg(new File(baseDirectory, BASENAME + ".tif").getPath(), m_sfimg);
+	}
+
+	@Test
+	public void tiffSequentialTest() throws ImgIOException,
+			IncompatibleTypeException, FormatException {
+
+		m_saver.saveImg(new File(baseDirectory, BASENAME + ".tif").getPath(), m_sfimg, m_sequentialConfig);
+	}
+
+
+//	TODO: Can't really benchmark these as they complain a lot about  'Expected positive value for PhysicalSizeY; got 0.0'
+	@Test
+	public void omeTiffSequentialTest() throws Exception{
+		m_saver.saveImg(new File(baseDirectory, BASENAME + ".ome.tif").getPath(), m_sfimg, m_sequentialConfig );
+	}
+
+
+	@Test
+	public void omeTiffNaiveTest() throws ImgIOException,
+			IncompatibleTypeException, FormatException {
+
+		m_saver.saveImg(new File(baseDirectory, BASENAME + ".ome.tif").getPath(), m_sfimg);
 	}
 
 	@Test
@@ -101,10 +126,24 @@ public class WriteTest {
 	}
 
 	@Test
+	public void pngSequentialTest() throws ImgIOException,
+			IncompatibleTypeException, FormatException {
+
+		m_saver.saveImg(new File(baseDirectory, BASENAME + ".png").getPath(), m_sfimg, m_sequentialConfig);
+	}
+
+	@Test
 	public void epsNaiveTest() throws ImgIOException,
 			IncompatibleTypeException, FormatException {
 
 		m_saver.saveImg(new File(baseDirectory, BASENAME + ".eps").getPath(), m_sfimg);
+	}
+
+	@Test
+	public void epsSequentialTest() throws ImgIOException,
+			IncompatibleTypeException, FormatException {
+
+		m_saver.saveImg(new File(baseDirectory, BASENAME + ".eps").getPath(), m_sfimg, m_sequentialConfig);
 	}
 
 	@Test
@@ -115,17 +154,23 @@ public class WriteTest {
 	}
 
 	@Test
+	public void icsSequentialTest() throws ImgIOException,
+			IncompatibleTypeException, FormatException {
+
+		m_saver.saveImg(new File(baseDirectory, BASENAME + ".ics").getPath(), m_sfimg, m_sequentialConfig);
+	}
+
+	@Test
 	public void jpgNaiveTest() throws ImgIOException,
 			IncompatibleTypeException, FormatException {
 
 		m_saver.saveImg(new File(baseDirectory, BASENAME + ".jpg").getPath(), m_sfimg);
 	}
 
-//	@Test
-//	public void jp2NaiveTest() throws ImgIOException,
-//			IncompatibleTypeException, FormatException {
-//
-//		m_saver.saveImg(BASEFOLDER + BASENAME + ".jp2", m_sfimg);
-//	}
+	@Test
+	public void jpgSequentialTest() throws ImgIOException,
+			IncompatibleTypeException, FormatException {
 
+		m_saver.saveImg(new File(baseDirectory, BASENAME + ".jpg").getPath(), m_sfimg, m_sequentialConfig);
+	}
 }
